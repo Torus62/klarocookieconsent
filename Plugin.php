92b8c0e7f5ffb650fefa60c04ec34da431d9e1ef
Ondrej Brinkel <ondrej@studiobosco.de>
2021-04-14 11:09:44 +0200

    injects scripts into head

--- Plugin.php
+++ Plugin.php
@@ -54,8 +54,8 @@ class Plugin extends PluginBase
 
         $pluginUrl = url('/plugins/studiobosco/klarocookieconsent');
 
-        Block::append('scripts', '<script type="text/javascript">' . $consentConfig . '</script>');
-        Block::append('scripts', '<script type="text/javascript" src="' . $pluginUrl . '/assets/klaro-no-css.js"></script>');
+        Block::append('styles', '<script type="text/javascript">' . $consentConfig . '</script>');
+        Block::append('styles', '<script defer type="text/javascript" src="' . $pluginUrl . '/assets/klaro-no-css.js"></script>');
 
         if (!$noCss) {
             Block::append('styles', '<link rel="stylesheet" type="text/css" href="' . $pluginUrl . '/assets/klaro.css" />');
